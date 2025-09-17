21152efc22ef1697027185e1d4ad614a90bbdc64
BayramUysalBey <bayramuysal@windowslive.com>
2025-09-17 12:46:58 +0300

    'Reverse String' done in python.

---
+++ reverse_string.py
@@ -0,0 +1,11 @@
+'''Your task is to reverse a given string.
+
+Some examples:
+
+Turn "stressed" into "desserts".
+Turn "strops" into "sports".
+Turn "racecar" into "racecar".'''
+
+# My solution
+def reverse(text):
+    return text[::-1]
