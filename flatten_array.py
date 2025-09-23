ef38ba6bc37e74d99857d202c62fe32421ee2f8d
BayramUysalBey <bayramuysal@windowslive.com>
2025-09-23 11:02:14 +0300

    'Flatten Array' done in python.

---
+++ flatten_array.py
@@ -0,0 +1,22 @@
+"""Take a nested array of any depth and return a fully flattened array.
+
+Note that some language tracks may include null-like values in the input array, and the way these values are represented varies by track. Such values should be excluded from the flattened array.
+
+Additionally, the input may be of a different data type and contain different types, depending on the track.
+
+Check the test suite for details.
+
+Example
+input: [1, [2, 6, null], [[null, [4]], 5]]
+
+output: [1, 2, 6, 4, 5]"""
+
+# My solution
+def flatten(iterable):
+    flattened = []
+    for item in iterable:
+        if isinstance(item, list):
+            flattened.extend(flatten(item))
+        elif item is not None:
+            flattened.append(item)
+    return flattened
